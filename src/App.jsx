b5b91fa32867c144c768da622de1cf0cd06780e7
Sebastien <fenelonsebastien@outlook.com>
2025-10-21 12:34:35 -0400

    new temp

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import Bento from "./templates/Bento"
 import Minimalism from "./templates/Minimalistic"
 import Glassmorphism from "./templates/Glassmorphism"
 
-
+import TemplateTester from "./templates/TemplateTester"
 
 export default function App() {
   return (
     <>
 
 {/* Comment Out or remove the template you want to hide from VIEW */}
-    <Bento/>
+    {/* <Bento/>
     <Minimalism/>
-    <Glassmorphism/>
+    <Glassmorphism/> */}
+    <TemplateTester/>
     </>
   )
 }
